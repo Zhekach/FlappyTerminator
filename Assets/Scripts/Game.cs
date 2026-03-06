91531526dd2c294524db7aa05e99619e362e7894
Zhekach <62952369+Zhekach@users.noreply.github.com>
2026-03-06 19:46:36 +0300

    add windows base

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private EnemyGenerator _enemyGenerator;
+
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Window : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private Button _button;
+    
+    public CanvasGroup CanvasGroup => _canvasGroup;
+    public Button Button => _button;
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+
+    protected abstract void OnButtonClick();
+    
+    protected abstract void Open();
+    
+    protected abstract void Close();
+}
