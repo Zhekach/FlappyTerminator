3ae976ac174051ed95470f4ac9555ed1507e9737
Zhekach <62952369+Zhekach@users.noreply.github.com>
2026-03-06 16:25:29 +0300

    add base movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+[RequireComponent(typeof(PlayerMover))]
+public class Player : MonoBehaviour
 {
+    [SerializeField] private PlayerMover _playerMover;
+
+    private void Awake()
+    {
+        _playerMover = GetComponent<PlayerMover>();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+    
+    [SerializeField] private float _tapForce;
+    [SerializeField] private float _horizontalSpeed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _minRotationZ;
+    [SerializeField] private float _maxRotationZ;
+    
+    private CustomPlayerInput _input;
+
+    private Quaternion _minRotation;
+    private Quaternion _maxRotation;
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _input = new CustomPlayerInput();
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+        _input.Player.Jump.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _input.Player.Jump.performed -= OnJump;
+        _input.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocityX = _horizontalSpeed;
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            _minRotation,
+            _rotationSpeed * Time.fixedDeltaTime);
+    }
+    
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, 0f);
+        _rigidbody.AddForce(Vector2.up * _tapForce, ForceMode2D.Impulse);
+        
+        transform.rotation = _maxRotation;
+    }
+}
