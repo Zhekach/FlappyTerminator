51047a868e5f047f8f4de798d24797b682a598bb
Zhekach <62952369+Zhekach@users.noreply.github.com>
2026-03-12 17:37:56 +0300

    add bullet base

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,33 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour, IInteractable
 {
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private bool _isActive;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isActive)
+            _rigidbody.linearVelocityX = _speed;
+    }
+
+    public void Activate(float speed)
+    {
+        _speed = speed;
+        _isActive = true;
+    }
+
+    public void ResetState()
+    {
+        _isActive = false;
+        _speed = 0f;
+    }
 }
